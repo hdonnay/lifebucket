63aa73e4bdaa6dde55ded10325346168fb52b8d1
Hank Donnay <hank@code42.com>
2013-01-09 00:04:26 -0600

    switch over to kyotocabinet

--- lifebucket.py
+++ lifebucket.py
@@ -1,54 +1,80 @@
 #!/usr/bin/python
-import psycopg2
-import psycopg2.extras
+from kyotocabinet import *
 from datetime import datetime
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, _app_ctx_stack, config
-from flask.ext.wtf import Form, TextField, TextAreaField, Required
+from flask.ext.wtf import Form, TextAreaField, TextField, validators, ValidationError
+import json, re
 
-class submit(Form):
-        key = TextField('Key', validators=[Required()])
-        value = TextAreaField('Value', validators=[Required()])
+class Config():
+    DB = "bucket.kct"
+    LENS_STOR = "lens.kch"
+    DEBUG = True
+    SECRET_KEY = 'development key'
+    USERNAME = 'admin'
+    PASSWORD = 'default'
+
+class Insert(Form):
+    value = TextAreaField('Value', validators=[validators.Required()])
+
+#def lensCheck(message=None):
+    #if message is None or re.match(_lens_pattern, message) is None:
+        #return False
+    #return True
+
+def lensCheck(form, field):
+    if re.match(_lens_pattern, field.data) is None:
+        raise ValidationError(u'Must Match Lens Pattern.')
+
+class View(Form):
+    lens = TextField('Lens', validators=[validators.Required(), lensCheck])
 
 # create our little application :)
 app = Flask(__name__)
-app.config.from_object('lifebucket.config')
-app.config.from_envvar('LIFEBUCKET_CONFIG')
+app.config.from_object('lifebucket.Config')
+#app.config.from_envvar('LIFEBUCKET_CONFIG')
+
+_lens_pattern = r'^\s*from\s+(.+)\s+to\s+(.+)\s+get\s+(.+)\s*$'
+OK = u'success'
+NO = u'failure'
 
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
     """
     top = _app_ctx_stack.top
-    if not hasattr(top, 'psql'):
-        top.psql = psycopg2.connect(app.config['DSN'])
-        psycopg2.extras.register_hstore(top.psql) # This is what forces psycopg2 to interface Dicts with hstores.
-    return top.psql
+    if not hasattr(top, 'db'):
+        top.db = DB()
+        top.db.open(app.config['DB'], DB.OWRITER | DB.OCREATE)
+    return top.db
 
-def init_db():
-    """Creates the database tables."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-#@app.teardown_appcontext
-def close_db_connection(exception):
-    """Closes the database again at the end of the request."""
+def get_lens():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
     top = _app_ctx_stack.top
-    if hasattr(top, 'psql'):
-        top.psql.close()
+    if not hasattr(top, 'lens'):
+        top.lens = DB()
+        top.lens.open(app.config['LENS_STOR'], DB.OWRITER | DB.OCREATE)
+    return top.lens
 
 @app.route('/')
 def index():
+    form = Insert()
     if form.validate_on_submit():
         flash("Success")
         return redirect(url_for("index"))
-    return render_template('index.html', form=submit())
+    return render_template('index.html', form=form)
 
 @app.route('/out')
 def output():
-    pass
+    form = View()
+    if form.validate_on_submit():
+        if post_lens()['status'] == OK:
+            flash("Success")
+        else:
+            flash("Something was wrong")
+        return redirect(url_for("output"))
+    return render_template('out.html', form=form)
 
 @app.route('/settings')
 def settings():
@@ -62,14 +88,26 @@ def copyright():
 def help():
     return render_template('help.html')
 
-@app.route('/api/post', methods=("POST", "PUT"))
-def post():
+@app.route('/api/value', methods=("POST", "PUT"))
+def post_value():
     with app.app_context():
         db = get_db()
-        db.cursor().execute("INSERT INTO data (time, kv) VALUES (%s, %s);", (datetime.utcnow(), request.args.to_dict()))
+        db.set(datetime.utcnow(), json.dumps(request.args.to_dict()['value']))
         db.commit()
-        return jsonify(status='success')
-    return jsonify(status='failure')
+        return jsonify(status=OK)
+    return jsonify(status=NO)
+
+@app.route('/api/lens', methods=("POST", "PUT"))
+def post_lens():
+    with app.app_context():
+        lens=request.args.get('lens', None)
+        (time_a, time_b, ret) = re.match(_lens_pattern, lens).group(1,2,3)
+        db = get_lens()
+        new_key=db.count()+1
+        db.set(new_key, "{0};{1};{2}".format(time_a, time_b, ret))
+        db.commit()
+        return jsonify(status=OK, lens=new_key)
+    return jsonify(status=NO)
 
 if __name__ == '__main__':
     app.run()
